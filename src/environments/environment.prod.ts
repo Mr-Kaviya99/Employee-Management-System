c2c7ec0f3b892d7141dcf0d49ab33fd8fde16a6e
Mr-Kaviya99 <kavinduhasintha9@gmail.com>
2024-04-23 14:34:15 +0530

    updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,16 +1,5 @@
 export const environment = {
   production: true,
-  // baseUrl: 'https://developersstackapi.com/api/v1/',
-  // simpleIp: 'https://developersstackapi.com/',
-  firebaseConfig : {
-    // apiKey: "AIzaSyAT86UNovPiHMSi1t_7H4tMNEw5dn0Gtbw",
-    // authDomain: "developers-stack-96863.firebaseapp.com",
-    // projectId: "developers-stack-96863",
-    // storageBucket: "developers-stack-96863.appspot.com",
-    // messagingSenderId: "833069664694",
-    // appId: "1:833069664694:web:954c3ee571091a2bced917",
-    // measurementId: "G-GFYCBJXD2W"
-  },
-/*  baseUrl:'http://192.168.8.175:8000/api/v1/',
-  simpleIp:'http://192.168.8.175:8000/'*/
+  baseUrl:'http://localhost:8000/api/v1/',
+  simpleIp:'http://localhost:8000/'
 };
